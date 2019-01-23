5eafde957c495f2d915597ebb30b7dc7e73bc70d
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-23 19:45:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,14 @@ katz_deli = []
 def line(people)
   if people = 0 
     puts "The line is currently empty."
+  else 
+    puts "The line is currently: #{people}."
 end 
 end
+
+def take_a_number(katz_deli, string)
+  katz_deli.each do |name|
+    puts 
 
 
 
